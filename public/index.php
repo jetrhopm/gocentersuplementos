94e68ffca45cb64292d9c6a0c19de4872282c491
jetrhopm <jetrhopm@gmail.com>
2026-06-28 00:55:32 -0600

    Make public index portable for Hostinger

--- public/index.php
+++ public/index.php
@@ -5,16 +5,31 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+$basePath = realpath(__DIR__.'/..');
+$serverBasePath = dirname(dirname(dirname(__DIR__))).'/gocentersuplementos';
+$configuredBasePath = getenv('LARAVEL_BASE_PATH') ?: null;
+
+if ($configuredBasePath && file_exists($configuredBasePath.'/vendor/autoload.php')) {
+    $basePath = rtrim($configuredBasePath, '/\\');
+} elseif (! file_exists($basePath.'/vendor/autoload.php') && file_exists($serverBasePath.'/vendor/autoload.php')) {
+    $basePath = $serverBasePath;
+}
+
+if (! $basePath || ! file_exists($basePath.'/vendor/autoload.php')) {
+    http_response_code(500);
+    exit('Laravel base path not found.');
+}
+
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = $basePath.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require $basePath.'/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once $basePath.'/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
